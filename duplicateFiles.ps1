2875f79aa58c2c11f18fca83add1e59cf5c9bcf7
Suresh L. Paul <sureshlazaruspaul@gmail.com>
2022-08-19 22:04:41 -0400

    Add files via upload

---
+++ duplicateFiles.ps1
@@ -0,0 +1,8 @@
+﻿# This powershell code duplicates your file into N files.
+# As with all powershell scripts, USE WITH CAUTION
+
+$NumArray = (1..40) # number of duplicates
+
+ForEach ($number in $numArray) {
+    Copy-Item "C:\RProg\examFile.Rmd" -Destination "C:\RProg\output\$number.Rmd" 
+}
