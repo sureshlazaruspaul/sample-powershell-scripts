dc0f2e6888b3e7bd5df61e5c4dbf0c593f98cc83
Suresh L. Paul <sureshlazaruspaul@gmail.com>
2022-08-20 10:57:29 -0400

    Add files via upload

---
+++ mergeAllTxtFiles.ps1
@@ -0,0 +1,6 @@
+﻿# This powershell code merges multiple text files into one file.
+# As with all powershell scripts, USE WITH CAUTION
+
+Get-ChildItem C:\RProg\textFiles -include *.txt -rec | 
+	ForEach-Object {gc $_; ""} | 
+	out-file C:\RProg\textFiles\output\merged.txt
